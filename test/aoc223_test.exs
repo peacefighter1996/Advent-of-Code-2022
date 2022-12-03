38d1dd25b0c405a948664a087037afef43d98719
I. Arbouw <ian-arbouw-1996@hotmail.com>
2022-12-03 18:40:51 +0100

    update test methode

--- test/aoc223_test.exs
+++ test/aoc223_test.exs
@@ -1,10 +1,19 @@
-
-defmodule Aoc221Test do
+defmodule Aoc223Test do
   use ExUnit.Case
-  doctest Aoc22d1
+  doctest Aoc22d3
 
   test "example 1 for question 1" do
     assert Aoc22d3.question1(Aoc22d3.data("data/test/day3/1.txt")) == 157
+  end
+
+  test "input result question 1" do
+    IO.puts("day 3 question 1: [#{Aoc22d3.question1(Aoc22d3.data("data/input/day3.txt"))}]")
+    assert true
+  end
+
+  test "input result question 2" do
+    IO.puts("day 3 question 2: [#{Aoc22d3.question2(Aoc22d3.data("data/input/day3.txt"))}]")
+    assert true
   end
 
   test "example 1 for question 2" do
