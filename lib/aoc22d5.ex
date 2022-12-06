3fd601be66b7beb7b63e50f2dce4161fa2973322
I. Arbouw <ian-arbouw-1996@hotmail.com>
2022-12-06 12:31:40 +0100

    attempt 1

--- lib/aoc22d5.ex
+++ lib/aoc22d5.ex
@@ -17,22 +17,23 @@ defmodule Aoc22d5 do
     [get_state(Enum.at(data,0)), get_instructions(Enum.at(data,1))]
   end
 
-  @spec get_state(String.t()):: integer
+  @spec get_state(String.t()):: list(list(String.t()))
   def get_state(data) do
+    IO.puts("Getting state")
     data = String.split(data, "\r\n")
     count = round((String.length(Enum.at(data,0)) +1)/4)
-    #row = Enum.take(data, data.length - 1)
-    #[row, count]
-    #create amount of arrays equal to the count
-    Enum.map(0..count-1, fn x-> Enum.map(0..length(data)-2, fn y -> String.at(Enum.at(data,y),1+4*x) end) end)
-    # filer " "
+    IO.puts("filling stacks")
+    range = 0..length(data)-2
+    Enum.map(data, fn x -> Enum.map(range, String.at(x,1+4*x)) end)
+
+     fn x-> Enum.map(range, fn y -> String.at(1+4*x) end) end)
     |> Enum.map(fn x ->Enum.filter(x, fn y -> y != " " end) end)
   end
 
   def get_instructions(data) do
+    IO.puts("Getting instructions")
     data = String.split(data, "\r\n")
     |> Enum.map(fn x -> String.split(x, " ") end)
-    # get all even count
     Enum.map(data, fn y -> Enum.map(1..5//2, fn x-> String.to_integer(Enum.at(y,x)) end) end)
   end
 
@@ -62,7 +63,9 @@ defmodule Aoc22d5 do
 
   def execute9000([], stacks) do stacks end
   def execute9000(instructions, stacks) do
+
     [a,b,c] = Enum.at(instructions, 0)
+    IO.puts("move #{a} from #{b} to #{c}, instructions left:#{length(instructions)}")
     stacks_c = Enum.reverse(Enum.take(Enum.at(stacks,b-1), a)) ++ Enum.at(stacks,c-1)
     stacks_b = Enum.drop(Enum.at(stacks,b-1), a)
     execute9000(Enum.drop(instructions,1),Enum.map(0..length(stacks)-1, fn x -> cond do
@@ -75,6 +78,7 @@ defmodule Aoc22d5 do
   def execute9001([], stacks) do stacks end
   def execute9001(instructions, stacks) do
     [a,b,c] = Enum.at(instructions, 0)
+    IO.puts("move #{a} from #{b} to #{c}, instructions left:#{length(instructions)}")
     stacks_c = Enum.take(Enum.at(stacks,b-1), a) ++ Enum.at(stacks,c-1)
     stacks_b = Enum.drop(Enum.at(stacks,b-1), a)
     execute9001(Enum.drop(instructions,1),Enum.map(0..length(stacks)-1, fn x -> cond do
