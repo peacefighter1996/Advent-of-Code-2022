e94d1ce47b358836b54a42708f12460db961371c
I. Arbouw <ian-arbouw-1996@hotmail.com>
2022-12-06 22:04:10 +0100

    speed up d6

--- lib/aoc22d6.ex
+++ lib/aoc22d6.ex
@@ -10,7 +10,7 @@ defmodule Aoc22d6 do
     data("data/input/day6.txt")
   end
 
-  @spec data(String.t()) :: string
+  @spec data(String.t()) :: list(integer)
   def data(file) do
     {:ok, data} = File.read(file)
     String.to_charlist(data)
@@ -20,7 +20,8 @@ defmodule Aoc22d6 do
     question1(data())
   end
   def question1(data) do
-    find_index(Enum.take(data, 4), Enum.drop(data, 4),4)
+    initmask(Enum.take(data, 4))
+    |> find_index(Enum.take(data, 4), Enum.drop(data, 4),4,false)
   end
 
   @doc """
@@ -30,21 +31,59 @@ defmodule Aoc22d6 do
     question2(data())
   end
   def question2(data) do
-    find_index(Enum.take(data, 14), Enum.drop(data, 14),14)
+    initmask(Enum.take(data, 14))
+    |> find_index(Enum.take(data, 14), Enum.drop(data, 14),14, false)
   end
 
   def check_match(mask, x) do
-    Enum.map(0..length(mask)-1, fn y -> Enum.at(mask,y) == Enum.at(mask,x) end)
+    Enum.map(x..length(mask)-1, fn y -> Enum.at(mask,y) == Enum.at(mask,x) end)
     |> Enum.filter(fn x -> x == true end)
   end
 
-  def find_index(mask,data, i) do
-    result = Enum.map(0..length(mask)-1,fn x-> check_match(mask,x) end) |> Enum.count(fn x -> x == [true] end)
-    if result == length(mask) do
-      i
+  def initmask(mask) do
+    initmask(%{},mask)
+  end
+  def initmask(maskmap,[]) do
+    maskmap
+  end
+  def initmask(maskmap,mask) do
+    if Map.has_key?(maskmap, Enum.at(mask,0)) do
+      {:ok,value} = Map.fetch(maskmap, Enum.at(mask,0))
+      Map.delete(maskmap, Enum.at(mask,0))
+      |> Map.put(Enum.at(mask,0), value+1)
+      |> initmask(Enum.drop(mask,1))
     else
-      newMask = Enum.drop(mask, 1) ++ [Enum.at(data,0)]
-      find_index(newMask, Enum.drop(data, 1), i+1)
+      Map.put(maskmap, Enum.at(mask,0),1)
+      |> initmask(Enum.drop(mask,1))
     end
+  end
+
+  def decrease(maskmap, x)do
+    {:ok,value} = Map.fetch(maskmap, x)
+    if value == 1 do
+      Map.delete(maskmap, x)
+    else
+      Map.delete(maskmap, x)
+      |> Map.put(x, value-1)
+    end
+  end
+
+  def increase(maskmap, x)do
+    if Map.has_key?(maskmap,x) do
+      {:ok,value} = Map.fetch(maskmap, x)
+      Map.delete(maskmap, x)
+      |> Map.put(x, value+1)
+    else
+      Map.put(maskmap, x,1)
+    end
+  end
+  def find_index(maskmap,mask,data, i, true) do
+    i
+  end
+  def find_index(maskmap,mask,data, i, false) do
+    maskmap
+    |> decrease(Enum.at(mask,0))
+    |> increase(Enum.at(data,0))
+    |> find_index(Enum.drop(mask, 1) ++ [Enum.at(data, 0)], Enum.drop(data, 1), i+1, length(Map.keys(maskmap)) == length(mask))
   end
 end
