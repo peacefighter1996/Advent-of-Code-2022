4d3c1aa486282fe86f190635dea47f445b026b68
I. Arbouw <ian-arbouw-1996@hotmail.com>
2022-12-07 19:41:43 +0100

    complete day 7

--- test/aoc227_test.exs
+++ test/aoc227_test.exs
@@ -6,17 +6,17 @@ defmodule Aoc22d7Test do
     assert Aoc22d7.question1(Aoc22d7.data("data/test/day7_1.txt")) == 95437
   end
 
-  #test "input result question 1" do
-  #  IO.puts("day 7 question 1: [#{Aoc22d7.question1(Aoc22d7.data("data/input/day7.txt"))}]")
-  #  assert true
-  #end
+  test "input result question 1" do
+    IO.puts("day 7 question 1: [#{Aoc22d7.question1(Aoc22d7.data("data/input/day7.txt"))}]")
+    assert true
+  end
 ##
-  #test "input result question 2" do
-  #  IO.puts("day 7 question 2: [#{Aoc22d7.question2(Aoc22d7.data("data/input/day7.txt"))}]")
-  #  assert true
-  #end
+  test "input result question 2" do
+    IO.puts("day 7 question 2: [#{Aoc22d7.question2(Aoc22d7.data("data/input/day7.txt"))}]")
+    assert true
+  end
 ##
-  #test "example 1 for question 2" do
-  #  assert Aoc22d7.question2('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 19
-  #end
+  test "example 1 for question 2" do
+    assert Aoc22d7.question2(Aoc22d7.data("data/test/day7_1.txt")) == 24933642
+  end
 end
---
+++ test/aoc22_test.exs
@@ -0,0 +1,10 @@
+defmodule Aoc223Test do
+  use ExUnit.Case
+  doctest Aoc22d3
+
+  test "example 1 for question 1" do
+    assert Aoc22.runAll()
+  end
+
+
+end
